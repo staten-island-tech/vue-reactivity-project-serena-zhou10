e9d05bf6ac2ccc8f73a5bf0462612d7ac0e6f302
Serena <serenazhou4268@gmail.com>
2026-02-09 12:31:48 -0500

    Update App.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,27 +2,14 @@
   <div>
     <h1>Pizza Builder</h1>
     <builder__form>
-      <form>
-
-      </form>
+      <form></form>
     </builder__form>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const animals = ref([
-  { name: 'Leo', species: 'Lion', weight: 420 },
-  { name: 'Ella', species: 'Elephant', weight: 5000 },
-  { name: 'Max', species: 'Dog', weight: 30 },
-  { name: 'Whiskers', species: 'Cat', weight: 4 },
-  { name: 'Bubbles', species: 'Fish', weight: 0.5 },
-  { name: 'Charlie', species: 'Horse', weight: 500 },
-  { name: 'Hoppy', species: 'Rabbit', weight: 3 },
-  { name: 'Tweety', species: 'Bird', weight: 0.2 },
-  { name: 'Gerry', species: 'Giraffe', weight: 1200 },
-  { name: 'Bella', species: 'Bear', weight: 300 },
-])</script>
+</script>
 
 <style scoped>
 div {
