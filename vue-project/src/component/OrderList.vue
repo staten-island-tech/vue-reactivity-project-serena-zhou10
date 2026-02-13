853f2591a29fd7fe1413310fa5e96ad8295b462d
Serena <serenazhou4268@gmail.com>
2026-02-13 12:32:13 -0500

    Update OrderList.vue

--- vue-project/src/component/OrderList.vue
+++ vue-project/src/component/OrderList.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-
+    <div class="order">
+      <h2 class="order__title">Your Order</h2>
+    </div>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped></style>
